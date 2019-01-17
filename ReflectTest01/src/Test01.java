9d04248288f2c808d1197bed044cbc4aeefa605a
xiehairui <1145993570@qq.com>
2019-01-17 23:19:12 +0800

    关于java中的可变长参数

---
+++ ReflectTest01/src/Test01.java
@@ -0,0 +1,28 @@
+/*
+    关于java中的可变长参数
+ */
+public class Test01 {
+    //m1方法有一个int类型可变长参数
+    public static void m1(int...a){
+        System.out.println("Test");
+    }
+    //如果可以精确匹配的方法，则调用该方法，不会再去执行可变长参数的那个方法
+    public static void m1(int i){
+        System.out.println(i);
+    }
+    //可变长参数可以等同看做数组
+    public static void m2(String...args){
+        for (int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+    public static void main(String[] args){
+        m1();
+        m1(1);
+        m1(1,2);
+        m1(3,4,5,6,7);
+
+        m2("体育","音乐","旅游","美食");
+    }
+
+}
