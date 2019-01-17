70747c1d8425135774742068dcc0558ae9a870f2
xiehairui <1145993570@qq.com>
2019-01-17 23:29:41 +0800

    关于java中的可变长参数

--- ReflectTest01/src/Test01.java
+++ ReflectTest01/src/Test01.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /*
     关于java中的可变长参数
  */
@@ -11,11 +13,18 @@ public class Test01 {
         System.out.println(i);
     }
     //可变长参数可以等同看做数组
+
     public static void m2(String...args){
         for (int i=0;i<args.length;i++){
             System.out.println(args[i]);
         }
     }
+    public static void m3(Class...args){
+        for (int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+   // public static void m4(String...a,int i){}//可变长参数只能出现一次，并且在最后一位出现
     public static void main(String[] args){
         m1();
         m1(1);
@@ -23,6 +32,7 @@ public class Test01 {
         m1(3,4,5,6,7);
 
         m2("体育","音乐","旅游","美食");
+        m3(Date.class,Employee.class);
     }
 
 }
